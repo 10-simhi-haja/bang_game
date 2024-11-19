cd4edcffffe6aafb19b0648a545fe2ff5c9cf5f7
Tcoat1 <wlsduqwkd8876@naver.com>
2024-11-19 21:34:25 +0900

    (김진엽)add:"방 입장"

--- src/handlers/index.js
+++ src/handlers/index.js
@@ -4,8 +4,9 @@ import config from '../config/config.js';
 import handlePositionUpdate from './syncCharacter/position.handler.js';
 import registerHandler from './user/registerHandler.js';
 import loginHandler from './user/loginHandler.js';
-import roomListHnadler from './room/roomListHandler.js';
-import createRoomHnadler from './room/createRoomHandler.js';
+import roomListHandler from './room/roomListHandler.js';
+import createRoomHandler from './room/createRoomHandler.js';
+import joinRoomHandler from './room/joinRoomHandler.js';
 
 const { packetType } = config.packet;
 
@@ -27,7 +28,7 @@ const handlers = {
     protoType: 'auth.S2CLoginResponse', ///
   },
   [packetType.CREATE_ROOM_REQUEST]: {
-    handler: createRoomHnadler,
+    handler: createRoomHandler,
     protoType: 'room.C2SCreateRoomRequest',
   },
   [packetType.CREATE_ROOM_RESPONSE]: {
@@ -35,7 +36,7 @@ const handlers = {
     protoType: 'room.S2CCreateRoomResponse',
   },
   [packetType.GET_ROOM_LIST_REQUEST]: {
-    handler: roomListHnadler,
+    handler: roomListHandler,
     protoType: 'room.C2SGetRoomListRequest',
   },
   [packetType.GET_ROOM_LIST_RESPONSE]: {
@@ -43,7 +44,7 @@ const handlers = {
     protoType: 'room.S2CGetRoomListResponse',
   },
   [packetType.JOIN_ROOM_REQUEST]: {
-    handler: undefined,
+    handler: joinRoomHandler,
     protoType: 'room.C2SJoinRoomRequest',
   },
   [packetType.JOIN_ROOM_RESPONSE]: {
--- src/handlers/room/createRoomHandler.js
+++ src/handlers/room/createRoomHandler.js
@@ -1,16 +1,16 @@
 import handleError from '../../utils/errors/errorHandler.js';
-import { v4 as uuidv4 } from 'uuid';
 import { createResponse } from '../../utils/packet/response/createResponse.js';
 import config from '../../config/config.js';
 import { addGameSession } from '../../sessions/game.session.js';
 import { getUserBySocket } from '../../sessions/user.session.js';
 
-const createRoomHnadler = async ({ socket, payload }) => {
+let count = 0;
+const createRoomHandler = async ({ socket, payload }) => {
   try {
     const { name, maxUserNum } = payload;
     let roomData = {
-      id: uuidv4(),
-      ownerId: getUserBySocket(socket).userId,
+      id: ++count,
+      ownerId: getUserBySocket(socket).id,
       name: name,
       maxUserNum: maxUserNum,
       state: config.roomStateType.wait,
@@ -43,4 +43,4 @@ const createRoomHnadler = async ({ socket, payload }) => {
   }
 };
 
-export default createRoomHnadler;
+export default createRoomHandler;
