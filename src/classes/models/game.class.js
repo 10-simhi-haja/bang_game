3441654686f25e7d77f19929f9f88d6e3822475a
김준영 <127184534+WaDDaDDa@users.noreply.github.com>
2024-11-19 11:02:10 +0900

    update game 클래스 추가 작업

--- src/classes/models/game.class.js
+++ src/classes/models/game.class.js
@@ -1,6 +1,7 @@
 // 핸들러 구현을 위해 임시로 작성한 로직, 돌아가는지 확인이 불가능 하기에 함수를 제외한 팀원들 코드로 대체 예정
 import User from './user.class.js';
 
+// game.users[userId] 로 해당 유저를 찾을 수 있다.
 class Game {
   constructor(roomData) {
     this.id = roomData.id;
@@ -8,7 +9,11 @@ class Game {
     this.name = roomData.name;
     this.maxUserNum = roomData.maxUserNum;
     this.state = roomData.state; // WAIT, PREPARE, INGAME
-    this.users = roomData.users.map((userData) => new User(userData));
+    this.users = {
+      length: 0,
+    };
+    // 인터버 매니저 추가되면.
+    // this.intervalManager = new IntervalManager();
   }
 
   getRoomData() {
@@ -21,17 +26,35 @@ class Game {
       users: this.users.map((user) => user.getUserData()),
     };
   }
+
   // 게임 상태 변경
   changeState(newState) {
     this.state = newState;
   }
 
   // 유저 추가
-  addUser(userData) {
+  addUser(user) {
     if (this.users.length >= this.maxUserNum) {
-      throw new Error('Room is full. Cannot add more users.');
+      throw new Error(
+        `방이 가득 찼습니다. 현재인원 : ${this.users.length}, 최대 인원 : ${this.maxUserNum}`,
+      );
     }
-    this.users.push(new User(userData));
+
+    this.users.length++;
+    this.users[user.id] = {
+      user,
+    };
+  }
+
+  removeUser(userId) {
+    if (this.users.length == 0) {
+      return;
+    }
+    delete this.users[userId];
+    this.users.length--;
+
+    // 인터버 매니져 추가되면.
+    // this.intervalManager.removePlayer(userId);
   }
 }
 
