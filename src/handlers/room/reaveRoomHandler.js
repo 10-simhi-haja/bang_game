b8e7f18bdcd4760d535186cfcb9c8a300f94d7ae
jini031104 <90189794+jini031104@users.noreply.github.com>
2024-11-19 20:27:47 +0900

    (정진희) update: "방 나가기 기능 추가"

--- src/handlers/room/reaveRoomHandler.js
+++ src/handlers/room/reaveRoomHandler.js
@@ -1,5 +1,26 @@
-const reaveRoomHandler = async ({ socket, payload }) => {
-  console.log('이것은 방 나가기다!');
+import config from '../../config/config.js';
+import { getUserBySocket } from '../../sessions/user.session.js';
+import handleError from '../../utils/errors/errorHandler.js';
+import { createResponse } from '../../utils/packet/response/createResponse.js';
+
+const leaveRoomHandler = async ({ socket, payload }) => {
+  try {
+    // 방 나가기 응답
+    const responseData = {
+      success: true,
+      failcode: 0,
+    };
+
+    const leaveRoomResponse = createResponse(
+      config.packet.packetType.LEAVE_ROOM_RESPONSE,
+      socket.sequence,
+      responseData,
+    );
+
+    socket.write(leaveRoomResponse);
+  } catch (error) {
+    handleError(socket, error);
+  }
 };
 
-export default reaveRoomHandler;
+export default leaveRoomHandler;
