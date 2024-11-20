4213e5c00cbc2dd63c93d4d6155a19df503626ee
jini031104 <90189794+jini031104@users.noreply.github.com>
2024-11-20 14:25:13 +0900

    (정진희) update: "주석 추가"

--- src/utils/notification/leaveRoomNotification.js
+++ src/utils/notification/leaveRoomNotification.js
@@ -3,15 +3,14 @@ import { createResponse } from '../packet/response/createResponse.js';
 
 // 방에 남은 유저들에게 전달
 const leaveRoomNotification = (socket, userId, room) => {
-  console.log('나가는 사람: ', userId);
-  console.log('room: ', room);
-
+  // 응답 패킷 생성
   const leaveRoomNotification = createResponse(
     config.packet.packetType.LEAVE_ROOM_NOTIFICATION,
     socket.sequence,
     userId,
   );
 
+  // 나를 제외한 = 방에 남은 유저들에게만 알림 전송
   Object.entries(room.users).forEach(([key, userData]) => {
     if (key !== userId.toString()) {
       const userSocket = userData.user.socket;
