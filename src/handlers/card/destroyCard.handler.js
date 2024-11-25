ea44320be6874c086f1ce35e65d19453beef9dbd
김준영 <127184534+WaDDaDDa@users.noreply.github.com>
2024-11-25 12:19:19 +0900

    김준영 add 카드버리기 핸들러 작업
    페이로드를 undefined로 받고있는 문제가 있음

--- src/handlers/card/destroyCard.handler.js
+++ src/handlers/card/destroyCard.handler.js
@@ -1,6 +1,8 @@
-import { getGameSessionBySocket } from '../../sessions/game.session.js';
+import { getGameSessionBySocket, getGameSessionByUser } from '../../sessions/game.session.js';
 import { createResponse } from '../../utils/packet/response/createResponse.js';
 import { PACKET_TYPE } from '../../constants/header.js';
+import handleError from '../../utils/errors/errorHandler.js';
+import { getUserBySocket } from '../../sessions/user.session.js';
 
 const packetType = PACKET_TYPE;
 
@@ -14,58 +16,22 @@ const packetType = PACKET_TYPE;
 // }
 
 // 카드 버리기 요청 핸들러
-const destroyCardRequest = async (socket, payload) => {
+const destroyCardRequest = (socket, payload) => {
   try {
-    const { destroyCards } = payload;
+    //const { destroyCards } = payload;
+    console.log(`카드버리기 ${payload}`);
 
-    const gameSession = getGameSessionBySocket(socket);
-    if (!gameSession) {
-      throw new Error('해당 유저의 게임 세션이 존재하지 않습니다.');
-    }
+    //const user = getUserBySocket(socket);
 
-    const currentUser = gameSession.users.find((user) => user.socket === socket);
-    if (!currentUser) {
-      throw new Error('현재 유저가 존재하지 않습니다.');
-    }
+    //const game = getGameSessionByUser(user);
 
-    // 버릴 카드 목록에서 카드들을 제거
-    destroyCards.forEach((destroyCard) => {
-      for (let i = 0; i < destroyCard.count; i++) {
-        const cardIndex = currentUser.character.handCards.findIndex(
-          (card) => card.type === destroyCard.type,
-        );
-        if (cardIndex !== -1) {
-          currentUser.character.handCards.splice(cardIndex, 1);
-        } else {
-          throw new Error(
-            `유저의 핸드에 ${destroyCard.type} 타입의 카드가 충분히 존재하지 않습니다.`,
-          );
-        }
-      }
-    });
+    // const destroyCardResponse = createResponse(packetType.DESTROY_CARD_RESPONSE, socket.sequence, {
+    //   handCards: remainingHandCards,
+    // });
 
-    // 현재 유저의 남은 카드 목록을 응답
-    const remainingHandCards = currentUser.character.handCards.map((card) => ({
-      type: card.type,
-      count: card.count,
-    }));
-
-    const destroyCardResponse = createResponse(packetType.DESTROY_CARD_RESPONSE, socket.sequence, {
-      handCards: remainingHandCards,
-    });
-
-    socket.write(destroyCardResponse);
+    // socket.write(destroyCardResponse);
   } catch (error) {
-    console.error('카드 버리기 중 에러 발생:', error.message);
-
-    // 요청을 보낸 소켓에 실패 여부 보내기
-    const errorResponse = createResponse(packetType.DESTROY_CARD_RESPONSE, socket.sequence, {
-      handCards: currentUser.character.handCards.map((card) => ({
-        type: card.type,
-        count: card.count,
-      })),
-    });
-    socket.write(errorResponse);
+    handleError(socket, error);
   }
 };
 
