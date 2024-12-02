f0b7db11294dc1a88678919c4e06cb30d28c2b16
김준영 <127184534+WaDDaDDa@users.noreply.github.com>
2024-12-02 16:51:13 +0900

    김준영 add 말랑이 피격시 드로우 추가

--- src/constants/header.js
+++ src/constants/header.js
@@ -94,12 +94,12 @@ export const CHARACTER_TYPE = {
   RED: 1, // 빨강이
   SHARK: 3, // 상어군
   MALANG: 5, // 말랑이
-  FROGGY: 7, // 개굴군
-  PINK: 8, // 핑크군
-  SWIM_GLASSES: 9, // 물안경군
-  MASK: 10, // 가면군
-  DINOSAUR: 12, // 공룡이
-  PINK_SLIME: 13, // 핑크슬라임
+  // FROGGY: 7, // 개굴군
+  // PINK: 8, // 핑크군
+  // SWIM_GLASSES: 9, // 물안경군
+  // MASK: 10, // 가면군
+  // DINOSAUR: 12, // 공룡이
+  // PINK_SLIME: 13, // 핑크슬라임
 };
 
 export const CARD_TYPE = {
