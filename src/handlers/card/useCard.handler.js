75033dabae3801f1aa43c64d6a3df0ca8bb6e562
jini031104 <90189794+jini031104@users.noreply.github.com>
2024-12-09 01:07:17 +0900

    (정진희) update: "CustomError에 사용되지 않았던 socket.sequence 추가"

--- src/handlers/card/useCard.handler.js
+++ src/handlers/card/useCard.handler.js
@@ -28,6 +28,7 @@ const useCardHandler = ({ socket, payload }) => {
     const targetId = targetUserId.low;
     const user = getUserBySocket(socket);
     const game = getGameSessionByUser(user);
+    console.log('혹시 이것도 문제야?: ', game);
     const users = game.getAllUserDatas();
     const targetIds = game.getLiveUsersId();
     const userId = user.id;
