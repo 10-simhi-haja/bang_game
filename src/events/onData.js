99733255eac03903197fa6cb1877077fac41a0f5
jini031104 <90189794+jini031104@users.noreply.github.com>
2024-12-13 21:38:10 +0900

    (정진희)tset

--- src/events/onData.js
+++ src/events/onData.js
@@ -63,13 +63,13 @@ const onData = (socket) => async (data) => {
       const sequence = socket.buffer.readUInt32BE(offset);
       offset += config.packet.sequenceLength;
       const isValidSequence = validateSequence(socket, sequence);
-      if (!isValidSequence) {
-        throw new CustomError(
-          ErrorCodes.INVALID_SEQUENCE,
-          `패킷이 중복되거나 누락되었다: 예상 시퀀스: ${socket.sequence + 1}, 받은 시퀀스: ${sequence}`,
-          socket.sequence,
-        );
-      }
+      // if (!isValidSequence) {
+      //   throw new CustomError(
+      //     ErrorCodes.INVALID_SEQUENCE,
+      //     `패킷이 중복되거나 누락되었다: 예상 시퀀스: ${socket.sequence + 1}, 받은 시퀀스: ${sequence}`,
+      //     socket.sequence,
+      //   );
+      // }
       // 6. 페이로드 길이 (4 bytes)
       const payloadLength = socket.buffer.readUInt32BE(offset);
       offset += config.packet.payloadLength;
