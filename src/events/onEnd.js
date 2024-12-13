b85ddca4a60c2115bfa813d75d6e0d7fee01b5a0
jini031104 <90189794+jini031104@users.noreply.github.com>
2024-12-13 11:15:33 +0900

    (정진희) fix: "게임 도중에 나가면 생겼던 문제 해결"
    게임 도중에 클라이언트 종료 시,
    1. 해당 캐릭터는 HP를 0으로 하여 사망처리 한다.
    2. 캐릭터&역할 분배 ~ 게임 중인 방에는 입장할 수 없다.

--- src/events/onEnd.js
+++ src/events/onEnd.js
@@ -6,6 +6,7 @@ import config from '../config/config.js';
 import CustomError from '../utils/errors/customError.js';
 import ErrorCodes from '../utils/errors/errorCodes.js';
 import handleError from '../utils/errors/errorHandler.js';
+import userUpdateNotification from '../utils/notification/userUpdateNotification.js';
 
 // ! 게임 중에 방장이 나가면 오류 발생!!!!
 // 종료되면 해당 유저 게임에서 제거하는 과정.
@@ -19,11 +20,9 @@ export const onEnd = (socket) => async () => {
     }
     const room = getGameSessionByUser(user);
 
-    // 유저가 게임에 속했을 경우
+    // 유저가 게임에 속하고 아직 게임 시작이 아닐 때
     if (room && room.state !== config.roomStateType.inGame) {
-      console.log('이건 뭘까?: ', room.state);
       room.removeUser(user.id);
-
       // 방장이 나갔을 경우
       // 방장이 나가면 모든 사람을 내쫒고 방은 삭제
       let usersLength = room.getUserLength();
@@ -53,6 +52,11 @@ export const onEnd = (socket) => async () => {
       );
 
       socket.write(leaveRoomResponse);
+    } else if (room && room.state === config.roomStateType.inGame) {
+      // 게임 중에 나가면 사망처리
+      const userCharacter = room.getCharacter(user.id);
+      userCharacter.hp = 0;
+      userUpdateNotification(room);
     }
     removeUserBySocket(socket);
   } catch (error) {
