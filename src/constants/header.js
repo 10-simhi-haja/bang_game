4bfb1b56e534fa365d2427f9ebdc334ac6c09510
rabbit-97 <uzam97@gmail.com>
2024-12-18 21:47:35 +0900

    (김우섭) fix: "합치기 전 수정"

--- src/constants/header.js
+++ src/constants/header.js
@@ -131,29 +131,6 @@ export const PHASE_TYPE = {
   END: 3,
 };
 
-// 인터벌 주기
-// 필요한 주기가 있으면 추가해서 사용.
-// 전부 초단위
-export const INTERVAL = {
-  SYNC_POSITION: 1,
-  SYNC_GAME: 2,
-  PHASE_UPDATE_DAY: 60,
-  PHASE_UPDATE_END: 60,
-  FLEA_MARKET_PICK: 10,
-  BOMB: 5,
-  BOMB_ANIMATION: 5,
-  ATTACK: 10,
-};
-
-export const INTERVAL_TYPE = {
-  POSITION: 0,
-  PHASE_UPDATE: 1,
-  GAME_UPDATE: 2,
-  CHARACTER_STATE: 3,
-  BOMB: 4,
-  BOMB_ANIMATION: 5,
-};
-
 export const WIN_TYPE = {
   TARGET_AND_BODYGUARD_WIN: 0,
   HITMAN_WIN: 1,
