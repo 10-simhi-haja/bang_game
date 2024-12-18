d1ee58b341053273be70e833d561ab663d659715
jini031104 <90189794+jini031104@users.noreply.github.com>
2024-12-18 22:49:23 +0900

    (정진희) test: "포트 수정"

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ import onConnection from './events/onConnection.js';
 import initServer from './init/index.js';
 import config from './config/config.js';
 
-const BASE_PORT = parseInt(process.env.BASE_PORT || 5555, 10); // 기본 시작 포트
+const BASE_PORT = parseInt(process.env.BASE_PORT || 5556, 10); // 기본 시작 포트
 const INSTANCE_OFFSET = parseInt(process.env.NODE_APP_INSTANCE || 0, 10); // PM2가 제공하는 인스턴스 번호
 const PORT = BASE_PORT + INSTANCE_OFFSET; // 각 인스턴스의 고유 포트
 
